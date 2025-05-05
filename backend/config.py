e4f12b132237413e011127e6ae51df56fca50b51
jesusgvqz <jesusgvqz@outlook.es>
2025-05-04 22:57:44 -0600

    load from .env file

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,14 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev_key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///dev.db")
+    SECRET_KEY = os.getenv("SECRET_KEY", "defaultsecretkey")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "defaultjwtkey")
+
+    JWT_ACCESS_TOKEN_EXPIRES = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 3600))
+    JWT_TOKEN_LOCATION = os.getenv("JWT_TOKEN_LOCATION", "headers").split(',')
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://nimbuscore:n1mbus.C0r3@db:5432/nimbuscoredb")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt_secret")
