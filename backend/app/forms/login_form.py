7f5a1d4a54cc91560ea961e20d61e569e3499f86
jesusgvqz <jesusgvqz@outlook.es>
2025-05-09 10:22:21 -0600

    initial migration to django

--- backend/app/forms/login_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField('Usuario', validators=[DataRequired()])
-    password = PasswordField('Contraseña', validators=[DataRequired()])
-    recaptcha = RecaptchaField()
-    submit = SubmitField('Iniciar sesión')
