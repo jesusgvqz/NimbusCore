3d55b03ad9fcbc47ec0bd0fd9934d51bec836e4d
jesusgvqz <jesusgvqz@outlook.es>
2025-05-09 15:34:43 -0600

    initial django secure settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,14 +14,13 @@ from pathlib import Path
 import environ
 import os
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 env = environ.Env(
     DEBUG=(bool, False)
 )
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -132,3 +131,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# COOKIES & SESSIONS
+SESSION_COOKIE_AGE = 3600  # 1 hora (en segundos)
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+SESSION_COOKIE_SECURE = True  # Solo enviar cookies de sesión por HTTPS (recomendado en producción)
+SESSION_COOKIE_SAMESITE = 'Strict'  # Evita que se usen en peticiones cross-site
+
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True  # Evita que JS pueda leer la cookie
+CSRF_COOKIE_SAMESITE = 'Strict'
