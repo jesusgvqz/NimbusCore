70a313518aea31ebc27f39b8049d3d1ac643c1c4
jesusgvqz <jesusgvqz@outlook.es>
2025-05-22 11:00:44 -0600

    feat: dashboard inital struct and urls

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login_view, name="login"),
-    path('dashboard/', views.menu_view, name='dashboard'),
-    path('opcion1/', views.menu_view, name='opcion1'),
-    path('opcion2/', views.menu_view, name='opcion2'),
-    path('opcion3/', views.menu_view, name='opcion3'),
-    path('opcion4/', views.menu_view, name='opcion4'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('opcion1/', views.dashboard_view, name='opcion1'),
+    path('opcion2/', views.dashboard_view, name='opcion2'),
+    path('opcion3/', views.dashboard_view, name='opcion3'),
+    path('opcion4/', views.dashboard_view, name='opcion4'),
 ]
--- backend/app/views.py
+++ backend/app/views.py
@@ -4,15 +4,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
 ## FORMS
-from backend.app.forms import LoginForm
-from backend.app.forms import ServerForm
-from backend.app.forms import ServiceForm
+from .forms import LoginForm
+from .forms import ServerForm
+from .forms import ServiceForm
 
 ## CIPHERS
-from backend.app.hashes import *
+from .hashes import *
 
 ## MODELS
-from backend.app.models import *
+from .models import *
 
 
 # VIEWS
@@ -22,6 +22,6 @@ def login_view(request):
     loginform = LoginForm()
     return render(request, "login.html", {"form": loginform})
 
-##DASHBOARD
+## DASHBOARD
 def dashboard_view(request):
     return render(request, 'dashboard.html')
