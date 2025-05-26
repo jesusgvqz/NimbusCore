2267ec8ae01b3642128ff60ef172dc4d70f05647
AxelGomez02 <axelmysterio619@hotmail.com>
2025-05-26 11:53:00 -0600

    xd

--- backend/app/views.py
+++ backend/app/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 #from django.views.decorators.csrf import csrf_protect
-
+a
 ## FORMS
 from .forms import LoginForm
 from .forms import ServerForm
