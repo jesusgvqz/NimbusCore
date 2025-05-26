119cafbe751f77c61a067003cc0749b7c209e3d1
jesusgvqz <jesusgvqz@outlook.es>
2025-05-26 11:10:32 -0600

    feat: Initial Usuario model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    nombre=models.CharField(max_length=50)
+    username=models.CharField(max_length=50, unique=True)
+    password=models.CharField(max_length=128)
+    salt=models.CharField(max_length=64, blank=True)
