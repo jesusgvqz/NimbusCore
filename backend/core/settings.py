501baf9e72b1798053789df696640ace503fdebd
jesusgvqz <jesusgvqz@outlook.es>
2025-06-01 20:07:01 -0600

    fix: static files using 'collectstatic'

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -129,9 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'app' / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_DIRS = [BASE_DIR / 'app' / 'static']
 
 
 # Default primary key field type
